f45b676fb520412a95713f52ee4f02a57276ba5a
Devi Veytia <deviveytia@hotmail.com>
2023-01-24 11:40:58 +0100

    add steps 2 and 3 to the make.R file

--- make.R
+++ make.R
@@ -28,5 +28,7 @@ pkgload::load_all(here::here())
 ## Run Project ----------
 
 source(knitr::purl(here::here("analyses","01-compile_test_list.Rmd"), output=tempfile()))
+source(knitr::purl(here::here("analyses","02-evaluate_comprehensiveness.Rmd"), output=tempfile()))
+source(knitr::purl(here::here("analyses","03-review_team_screening_training.Rmd"), output=tempfile()))
 
 
