69dcbd0269b2cb4b5385da93e36488530ff81eac
Devi Veytia <deviveytia@hotmail.com>
2023-01-24 11:05:10 +0100

    Use rcompendium to make folders to organize analysis; start by creating file to compile the test list

---
+++ make.R
@@ -0,0 +1,32 @@
+#' Systematic map on ocean-related options
+#' 
+#' @description 
+#' This project documents the steps for searching the literature and evaluating the search string, 
+#' training the review team in screening and coding and managing the references from
+#' the search results and the screening and coding results
+#' 
+#' @author Devi Veytia \email{devi.veytia@fondationbiodiversite.fr}
+#' 
+#' @date 2023/01/24
+
+
+## Install dependencies
+
+if (!("remotes" %in% installed.packages()[ , "Package"]))
+  install.packages("remotes")
+
+remotes::install_deps(upgrade = "never")
+# renv::restore()
+
+
+
+## Load Project Addins (R Functions and Packages) ----
+
+pkgload::load_all(here::here())
+
+
+## Run Project ----------
+
+source(knitr::purl(here::here("analyses","01-compile_test_list.Rmd"), output=tempfile()))
+
+
