a59f3d49b4a1e1a58b86372e1c02f8c650bbee95
Devi Veytia <deviveytia@hotmail.com>
2023-02-01 18:45:07 +0100

    write code for parallelized de-duping on rossi

---
+++ analyses/ross_dedup.R
@@ -0,0 +1,22 @@
+load("columns-for-deduplication.RData")
+
+years = sort(unique(dedupInfo$'year'))
+
+results = parallel::mclapply(1:length(years), function(i) {
+  
+  dedupInfo_sub = dedupInfo[dedupInfo$year == years[i], ]
+  
+  unique_title_id = revtools::find_duplicates(dedupInfo_sub,
+                            match_variable = "title", 
+                            match_function = "stringdist", 
+                            method = "osa", threshold = 5,
+                            remove_punctuation = TRUE, to_lower = TRUE)
+  
+  dedupInfo_sub$duplicate_id <- unique_title_id
+  return(dedupInfo_sub)
+})
+
+
+dedupInfo = do.call(rbind.data.frame, dedupInfo)
+
+save(dedupInfo, "title_dedup_ids.RData")
